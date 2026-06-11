4b9d00d047e60fbbf191a09b37e79cb40b464290
Mehranmir1750 <133797630+Mehranmir1750@users.noreply.github.com>
2026-06-11 21:03:31 +0530

    making chnages to te code adding functionalities

--- frontend/src/components/Details.jsx
+++ frontend/src/components/Details.jsx
@@ -6,39 +6,78 @@ export default function Details(
     // {detail}
 ){
 
-      const detail = {
+      const detail = [
+        {
+          date: '12/05/2026',
     item: "Iron Nails",
     quantity: 4,
     price: 300
-  };
+        },
+         {
+          date: '04/06/2026',
+    item: "Pipe",
+    quantity: 2,
+    price: 800
+        },
+         {
+          date: '11/06/2026',
+    item: "Hammer",
+    quantity: 1,
+    price: 500
+        }
+      ]
+
+
+      const balance = detail.reduce(
+        (sum,item) => sum + item.price * item.quantity, 0
+      )
     return (
         <>
 
         <Navbar/>
 
         <div className="detail_component">
+  <h2 className="detail_heading">Purchase Details</h2>
 
+  <table className="details_table">
+    <thead>
+      <tr>
+        <th>Date</th>
+        <th>Item</th>
+        <th>Quantity</th>
+        <th>Price</th>
+        <th>Total</th>
+      </tr>
+    </thead>
 
-             <h6 className="item_details">
-  <strong>Date</strong> {detail?.date}
-</h6>
-            
+    <tbody>
+  {detail.map((item, index) => (
+    <tr key={index}>
+      <td>{item.date}</td>
+      <td>{item.item}</td>
+      <td>{item.quantity}</td>
+      <td>₹{item.price}</td>
+      <td>₹{item.price * item.quantity}</td>
+    </tr>
+  ))}
 
-            <h6 className="item_details">
-  <strong>Item</strong> {detail?.item}
-</h6>
+  <tr>
+    <td colSpan="4">
+      <strong>Paid Amount</strong>
+    </td>
+    <td>₹1000</td>
+  </tr>
 
-<h6 className="item_details">
-  <strong>Quantity</strong> {detail?.quantity}
-</h6>
+  <tr>
+    <td colSpan="4">
+      <strong>Outstanding Balance</strong>
+    </td>
+    <td>₹{balance - 1000}</td>
+  </tr>
+</tbody>
 
-<h6 className="item_details">
-  <strong>Price</strong> ₹{detail?.price}
-</h6>
-<h6 className="item_details">
-  <strong>Total</strong> ₹{detail?.price*detail?.quantity}
-</h6>
-        </div>
+  </table>
+</div>
         
         
         
