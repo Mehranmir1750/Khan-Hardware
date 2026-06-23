53cec6dc326722f664f867ae2c33b90aa504ccfb
Mehranmir1750 <133797630+Mehranmir1750@users.noreply.github.com>
2026-06-23 21:03:15 +0530

    fixed auth error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import Home from './pages/Home'
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 // import Details from './components/Details';
 import Details from './pages/Details';
@@ -11,26 +11,64 @@ import Customer from './pages/Customer';
 import Transactions from './pages/Transaction';
 import AddTransaction from './pages/AddTransaction';
 import Edit from './pages/Edit';
+import { Navigate } from "react-router-dom";
 
 function App() {
+
+  // Add this component at the top of App.jsx
+const ProtectedRoute = ({ element }) => {
+  const token = localStorage.getItem("token");
+  return token ? element : <Navigate to="/login" />;
+};
 
   return (
     <>
 
-     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/details" element={<Details />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/admin-dashboard" element={<Admin />} />
-      <Route path="/add-customer" element={<AddCustomer />} />
-      <Route path='/Customer' element={<Customer/>} />
-      <Route path='/Transactions' element={<Transactions/>} />
-      <Route path='/add-transactions' element={<AddTransaction/>} />
-      <Route path='/logout' element={<Home/>} />
-      <Route path="/details/:id" element={<Details />}/>
-      <Route path="/edit-transaction/:id" element={<Edit />}/>
+    <Routes>
 
-    </Routes>
+  {/* Public Routes */}
+  <Route path="/" element={<Home />} />
+  <Route path="/login" element={<Login />} />
+  <Route path="/details" element={<Details />} />
+  <Route path="/details/:id" element={<Details />} />
+
+  {/* Protected Routes */}
+  <Route
+    path="/admin-dashboard"
+    element={<ProtectedRoute element={<Admin />} />}
+  />
+
+  <Route
+    path="/add-customer"
+    element={<ProtectedRoute element={<AddCustomer />} />}
+  />
+
+  <Route
+    path="/customer"
+    element={<ProtectedRoute element={<Customer />} />}
+  />
+
+  <Route
+    path="/transactions"
+    element={<ProtectedRoute element={<Transactions />} />}
+  />
+
+  <Route
+    path="/add-transactions"
+    element={<ProtectedRoute element={<AddTransaction />} />}
+  />
+
+  <Route
+    path="/edit-transaction/:id"
+    element={<ProtectedRoute element={<Edit />} />}
+  />
+
+  <Route
+    path="/logout"
+    element={<ProtectedRoute element={<Home />} />}
+  />
+
+</Routes>
  
      
     </>
